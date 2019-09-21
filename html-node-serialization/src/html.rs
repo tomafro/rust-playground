76c10219012d99b4f369ba438b4e880b077ab1c7
Tom Ward <tom@popdog.net>
2019-09-21 15:16:58 +0100

    Split html and serialization into their own modules

---
+++ html-node-serialization/src/html.rs
@@ -0,0 +1,39 @@
+mod serialize;
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Node {
+    Element(Element),
+    Text(Text),
+}
+
+#[derive(Debug, Default)]
+pub struct Element {
+    name: String,
+    attributes: HashMap<String, String>,
+    children: Vec<Node>,
+}
+
+#[derive(Debug)]
+pub struct Text {
+    text: String,
+}
+
+pub fn element(
+    name: impl Into<String>,
+    attributes: HashMap<String, String>,
+    children: Vec<Node>,
+) -> Node {
+    Node::Element(Element {
+        name: name.into(),
+        attributes,
+        children,
+    })
+}
+
+pub fn text(string: impl Into<String>) -> Node {
+    Node::Text(Text {
+        text: string.into(),
+    })
+}
---
+++ html-node-serialization/src/html/serialize.rs
@@ -0,0 +1,98 @@
+use serde::ser::SerializeMap;
+use serde::{Serialize, Serializer};
+
+use super::*;
+
+impl serde::Serialize for Node {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Node::Element(element) => element.serialize(serializer),
+            Node::Text(text) => text.serialize(serializer),
+        }
+    }
+}
+
+struct AttributesAndChildren<'a> {
+    attributes: &'a HashMap<String, String>,
+    children: &'a Vec<Node>,
+}
+
+impl<'a> serde::Serialize for AttributesAndChildren<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(self.attributes.len() + 1))?;
+
+        for (attribute, value) in self.attributes {
+            map.serialize_entry(&format!("@{}", attribute), value)?;
+        }
+
+        if self.children.len() > 0 {
+            map.serialize_entry("children", self.children)?;
+        }
+
+        map.end()
+    }
+}
+
+impl serde::Serialize for Element {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(1))?;
+        let attributes_and_children = AttributesAndChildren {
+            attributes: &self.attributes,
+            children: &self.children,
+        };
+        map.serialize_entry(&self.name, &attributes_and_children)?;
+        map.end()
+    }
+}
+
+impl serde::Serialize for Text {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(1))?;
+        map.serialize_entry("text!", &self.text)?;
+        map.end()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! hashmap {
+        ($( $key: expr => $val: expr ),*) => {{
+            let mut map = ::std::collections::HashMap::new();
+            $( map.insert($key, $val); )*
+            map
+        }}
+    }
+
+    #[test]
+    fn serialize_element() {
+        let attributes = hashmap!["class".to_string() => "playground".to_string()];
+        let children: Vec<Node> = vec![text("Hello World")];
+        let node = element("html", attributes, children);
+        let json = serde_json::to_string(&node).unwrap();
+        assert_eq!(
+            r#"{"html":{"@class":"playground","children":[{"text!":"Hello World"}]}}"#,
+            json
+        );
+    }
+
+    #[test]
+    fn serialize_text() {
+        let node = text("Hello World!");
+        let json = serde_json::to_string(&node).unwrap();
+        assert_eq!(r#"{"text!":"Hello World!"}"#, json);
+    }
+}

d9a5a2bfe47cec508015000011d254430b46eff1
Tom Ward <tom@popdog.net>
2019-09-22 15:29:32 +0100

    Use RefCell to mutate 'cached' answers

--- interior-mutability/src/lib.rs
+++ interior-mutability/src/lib.rs
@@ -1,18 +1,39 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 
 #[derive(Default)]
-struct Colours {
-    colours: HashMap<String, String>
+struct ValueExample {
+    value: i64,
+    negative: RefCell<Option<i64>>
 }
 
-impl Colours {
-    fn new() -> Colours {
-        let colours: HashMap<String, String> = HashMap::new();
-        Colours { colours }
+impl ValueExample {
+    fn new(value: i64) -> ValueExample {
+        let negative = RefCell::new(None);
+        ValueExample { value, negative }
     }
 
-    fn get(&self, name: &str) -> Option<&String> {
-        self.colours.get(name)
+    fn negative(&self) -> i64 {
+        *self.negative.borrow_mut().get_or_insert_with(|| -self.value )
+    }
+}
+
+struct HashMapExample {
+    cache: RefCell<HashMap<String, i64>>
+}
+
+impl HashMapExample {
+    fn new() -> HashMapExample {
+        let map: HashMap<String, i64> = HashMap::new();
+        let cache = RefCell::new(map);
+        HashMapExample { cache }
+    }
+
+    fn get(&self, key: &str) -> i64 {
+        *self.cache.borrow_mut().entry(key.to_owned()).or_insert_with(|| {
+            let value: i64 = key.parse().unwrap();
+            -value
+        })
     }
 }
 
@@ -22,8 +43,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let colours = Colours::new();
-        colours.get("hello");
+    fn calculates_negative_on_demand() {
+        let example = ValueExample::new(1);
+        assert_eq!(-1, example.negative());
+        assert_eq!(-1, example.negative());
+    }
+
+    #[test]
+    fn hashmap_example_on_demand() {
+        let example = HashMapExample::new();
+        assert_eq!(-5, example.get("5"));
+        assert_eq!(-5, example.get("5"));
     }
 }
